956cc611157774a162df7b72c567b4499b2af4e8
kimjihyo <kimjihyo0325@gmail.com>
2024-06-06 13:49:54 +0000

    7662.cpp

--- boj/7662/solution.cpp
+++ boj/7662/solution.cpp
@@ -1,60 +1,101 @@
+// min-max heap
+// odd level is min heap
+// even level is max heap
 #include <cmath>
 #include <iostream>
 #include <utility>
 using namespace std;
 
-void heapifyDown(int index);
 void minHeapifyDown(int index);
 void maxHeapifyDown(int index);
+void heapifyDown(int index);
 
-int count = 0;               // the number of elements in the heap
-long long heap[1'000'001]{}; // the heap
+int count = 0;
+long long arr[1'000'001];
+
+void printArr() {
+  for (int i = 1; i < 10; i++) {
+    cout << arr[i] << ' ';
+  }
+  cout << '\n';
+}
 
 int getLevel(int index) { return log2(index); }
+bool isMinLevel(int index) { return getLevel(index) % 2 == 0; }
 
-int findSmallestDescent(int index) {
-  int left = index * 2;
-  int right = index * 2 + 1;
-  int grandLeft = index * 4;
-  int grandRight = index * 4 + 1;
-  int smallest = index;
-  if (left <= count && heap[left] < heap[smallest]) {
-    smallest = left;
+void minHeapifyUp(int index) {
+  if (index < 4)
+    return;
+  if (arr[index] < arr[index / 4]) {
+    swap(arr[index], arr[index / 4]);
+    minHeapifyUp(index / 4);
   }
-  if (right <= count && heap[right] < heap[smallest]) {
-    smallest = right;
-  }
-  if (grandLeft <= count && heap[grandLeft] < heap[smallest]) {
-    smallest = grandLeft;
-  }
-  if (grandRight <= count && heap[grandRight] < heap[smallest]) {
-    smallest = grandRight;
-  }
-  return smallest;
 }
-int findLargestDescent(int index) {
-  int left = index * 2;
-  int right = index * 2 + 1;
-  int grandLeft = index * 4;
-  int grandRight = index * 4 + 1;
-  int largest = index;
-  if (left <= count && heap[left] > heap[largest]) {
-    largest = left;
+
+void maxHeapifyUp(int index) {
+  if (index < 4)
+    return;
+  if (arr[index] > arr[index / 4]) {
+    swap(arr[index], arr[index / 4]);
+    maxHeapifyUp(index / 4);
   }
-  if (right <= count && heap[right] > heap[largest]) {
-    largest = right;
+}
+
+void heapifyUp(int index) {
+  if (index == 1)
+    return;
+  if (isMinLevel(index)) {
+    if (arr[index] > arr[index / 2]) {
+      swap(arr[index], arr[index / 2]);
+      maxHeapifyUp(index / 2);
+    } else {
+      minHeapifyUp(index);
+    }
+  } else {
+    if (arr[index] < arr[index / 2]) {
+      swap(arr[index], arr[index / 2]);
+      minHeapifyUp(index / 2);
+    } else {
+      maxHeapifyUp(index);
+    }
   }
-  if (grandLeft <= count && heap[grandLeft] > heap[largest]) {
-    largest = grandLeft;
+}
+
+int getSmallest(int index) {
+  int resultIndex = index * 2;
+  long long resultValue = arr[index * 2];
+  int indices[] = {
+      index * 2 + 1, index * 4, index * 4 + 1, index * 4 + 2, index * 4 + 3,
+  };
+
+  for (auto i : indices) {
+    if (i <= count) {
+      resultValue = min(resultValue, arr[i]);
+      if (resultValue == arr[i])
+        resultIndex = i;
+    }
   }
-  if (grandRight <= count && heap[grandRight] > heap[largest]) {
-    largest = grandRight;
+  return resultIndex;
+}
+int getLargest(int index) {
+  int resultIndex = index * 2;
+  long long resultValue = arr[index * 2];
+  int indices[] = {
+      index * 2 + 1, index * 4, index * 4 + 1, index * 4 + 2, index * 4 + 3,
+  };
+
+  for (auto i : indices) {
+    if (i <= count) {
+      resultValue = max(resultValue, arr[i]);
+      if (resultValue == arr[i])
+        resultIndex = i;
+    }
   }
-  return largest;
+  return resultIndex;
 }
 
 void heapifyDown(int index) {
-  if (getLevel(index) % 2 == 0) {
+  if (isMinLevel(index)) {
     minHeapifyDown(index);
   } else {
     maxHeapifyDown(index);
@@ -64,89 +105,46 @@ void heapifyDown(int index) {
 void minHeapifyDown(int index) {
   if (index * 2 > count)
     return;
-  int smallest = findSmallestDescent(index);
+  int smallest = getSmallest(index);
   if (smallest >= index * 4) {
-    if (heap[smallest] < heap[index]) {
-      swap(heap[smallest], heap[index]);
-      index = smallest;
-      if (heap[index] > heap[index / 2]) {
-        swap(heap[index], heap[index / 2]);
-        index = index / 2;
+    if (arr[smallest] < arr[index]) {
+      swap(arr[smallest], arr[index]);
+      if (arr[smallest] > arr[smallest / 2]) {
+        swap(arr[smallest], arr[smallest / 2]);
       }
-      heapifyDown(index);
+      minHeapifyDown(smallest);
     }
-  } else if (smallest >= index * 2) {
-    if (heap[smallest] < heap[index]) {
-      swap(heap[smallest], heap[index]);
-    }
+  } else if (arr[smallest] < arr[index]) {
+    swap(arr[smallest], arr[index]);
   }
 }
 
 void maxHeapifyDown(int index) {
   if (index * 2 > count)
     return;
-  int largest = findLargestDescent(index);
+  int largest = getLargest(index);
   if (largest >= index * 4) {
-    if (heap[largest] > heap[index]) {
-      swap(heap[largest], heap[index]);
-      index = largest;
-      if (heap[index] < heap[index / 2]) {
-        swap(heap[index], heap[index / 2]);
-        index = index / 2;
+    if (arr[largest] > arr[index]) {
+      swap(arr[largest], arr[index]);
+      if (arr[largest] < arr[largest / 2]) {
+        swap(arr[largest], arr[largest / 2]);
       }
-      heapifyDown(index);
+      maxHeapifyDown(largest);
     }
-  } else if (largest >= index * 2) {
-    if (heap[largest] > heap[index]) {
-      swap(heap[largest], heap[index]);
-    }
-  }
-}
-
-void minHeapifyUp(int index) {
-  if (index < 4)
-    return;
-  if (heap[index] < heap[index / 4]) {
-    swap(heap[index], heap[index / 4]);
-    minHeapifyUp(index / 4);
-  }
-}
-
-void maxHeapifyUp(int index) {
-  if (index < 4)
-    return;
-  if (heap[index] > heap[index / 4]) {
-    swap(heap[index], heap[index / 4]);
-    maxHeapifyUp(index / 4);
+  } else if (arr[largest] > arr[index]) {
+    swap(arr[largest], arr[index]);
   }
 }
 
 void insert(long long value) {
-  heap[++count] = value;
-  if (count == 1)
-    return;
-  int index = count;
-  if (getLevel(index) % 2 == 0) {
-    if (heap[index] > heap[index / 2]) {
-      swap(heap[index], heap[index / 2]);
-      maxHeapifyUp(index / 2);
-    } else {
-      minHeapifyUp(index);
-    }
-  } else {
-    if (heap[index] < heap[index / 2]) {
-      swap(heap[index], heap[index / 2]);
-      minHeapifyUp(index / 2);
-    } else {
-      maxHeapifyUp(index);
-    }
-  }
+  arr[++count] = value;
+  heapifyUp(count);
 }
 
 void popMin() {
   if (count == 0)
     return;
-  heap[1] = heap[count--];
+  arr[1] = arr[count--];
   heapifyDown(1);
 }
 
@@ -158,33 +156,33 @@ void popMax() {
     index = 1;
   if (count == 2)
     index = 2;
-  else {
-    if (heap[2] > heap[3])
+  if (count >= 3) {
+    if (arr[2] > arr[3]) {
       index = 2;
-    else
+    } else {
       index = 3;
+    }
   }
-  heap[index] = heap[count--];
+  arr[index] = arr[count--];
   heapifyDown(index);
 }
 
-long long seekMin() { return heap[1]; }
+long long seekMin() { return arr[1]; }
 
 long long seekMax() {
-  if (count == 0 || count == 1)
-    return heap[1];
+  int index = 1;
+  if (count == 1)
+    index = 1;
   if (count == 2)
-    return heap[2];
-  else {
-    return max(heap[2], heap[3]);
+    index = 2;
+  if (count >= 3) {
+    if (arr[2] > arr[3]) {
+      index = 2;
+    } else {
+      index = 3;
+    }
   }
-}
-
-void print() {
-  for (int i = 0; i < 15; i++) {
-    cout << heap[i] << ' ';
-  }
-  cout << '\n';
+  return arr[index];
 }
 
 int main() {
@@ -192,29 +190,27 @@ int main() {
   cin.tie(0);
   int t;
   cin >> t;
-  for (int t0 = 0; t0 < t; t0++) {
+  for (int i = 0; i < t; i++) {
     int q;
     cin >> q;
-    for (int i = 0; i < q; i++) {
-      char o;
-      long long val;
-      cin >> o >> val;
-      if (o == 'I') {
-        insert(val);
-      }
-      if (o == 'D') {
-        if (val == 1) {
-          popMax();
-        } else {
+    for (int j = 0; j < q; j++) {
+      char a;
+      long long b;
+      cin >> a >> b;
+      if (a == 'I') {
+        insert(b);
+      } else if (a == 'D') {
+        if (b < 0) {
           popMin();
+        } else {
+          popMax();
         }
       }
     }
-    if (count == 0) {
+    if (count == 0)
       cout << "EMPTY\n";
-    } else {
+    else
       cout << seekMax() << ' ' << seekMin() << '\n';
-    }
     count = 0;
   }
   return 0;
