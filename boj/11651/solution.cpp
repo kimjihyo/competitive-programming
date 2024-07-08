edef5c1b9923079f2f4d4943c2686e6a6cefcae1
kimjihyo <kimjihyo0325@gmail.com>
2024-07-08 05:50:23 +0000

    ac

---
+++ boj/11651/solution.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool compare(pair<int, int> &a, pair<int, int> &b) {
+  if (a.second == b.second) {
+    return a.first < b.first;
+  }
+  return a.second < b.second;
+}
+
+int main() {
+  vector<pair<int, int>> arr;
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    int x, y;
+    cin >> x >> y;
+    arr.push_back({x, y});
+  }
+  sort(arr.begin(), arr.end(), compare);
+  for (auto e : arr) {
+    cout << e.first << ' ' << e.second << '\n';
+  }
+  return 0;
+}
