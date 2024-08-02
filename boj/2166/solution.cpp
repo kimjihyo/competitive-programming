e999494f9d9b154cc8f37e122415e010003ab55f
kimjihyo <kimjihyo0325@gmail.com>
2024-08-02 13:00:02 +0000

    ac

--- boj/2166/solution.cpp
+++ boj/2166/solution.cpp
@@ -1,14 +1,13 @@
-#include <cmath>
-#include <cstdlib>
+#include <ios>
 #include <iostream>
 #include <vector>
 using namespace std;
 
 struct Point {
-  long long x, y;
+  long double x, y;
 };
 
-long long ccw(Point p1, Point p2, Point p3) {
+long double ccw(Point p1, Point p2, Point p3) {
   return (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
 }
 
@@ -17,26 +16,21 @@ int main() {
   vector<Point> points;
   cin >> n;
   Point origin;
-  long long area = 0;
+  long double area = 0;
 
   for (int i = 0; i < n; i++) {
-    long long x, y;
+    long double x, y;
     cin >> x >> y;
     points.push_back({x, y});
 
     if (i > 1) {
-      area += ccw(points[0], points[i - 1], points[i]);
+      area += ccw(points[0], points[i - 1], points[i]) / 2;
     }
   }
 
-  // cout << round(10 * area) / 10 << '\n';
-  // cout.precision(1);
-  // cout << abs(area) << '\n';
-  if (area % 2 == 1) {
-    cout << abs(area / 2) << ".5\n";
-  } else {
-    cout << abs(area / 2) << '\n';
-  }
+  cout << fixed;
+  cout.precision(1);
+  cout << abs(area) << '\n';
 
   return 0;
 }
