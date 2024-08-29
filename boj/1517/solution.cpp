7baded19a47ffac0005a03baf623f9415d7f2e65
kimjihyo <kimjihyo0325@gmail.com>
2024-08-29 02:38:18 +0000

    ac

--- boj/1517/solution.cpp
+++ boj/1517/solution.cpp
@@ -3,14 +3,17 @@
 using namespace std;
 
 int n;
-pair<int, int> arr[500'000]{};
-int tree[1'500'000]{};
+pair<int, int> arr[500]{};
+long long tree[1'500]{};
 
 bool compare(pair<int, int> &a, pair<int, int> &b) {
+  if (a.second == b.second) {
+    return a.first < b.first;
+  }
   return a.second < b.second;
 }
 
-int get(int start, int end, int node, int from, int to) {
+long long get(int start, int end, int node, int from, int to) {
   if (from > end || to < start) {
     return 0;
   }
@@ -29,8 +32,22 @@ void update(int start, int end, int node, int index, int value) {
     return;
   }
   if (start == end) {
-    tree[node] = 1;
+    tree[node] = value;
+    return;
   }
+  int mid = (start + end) / 2;
+  int leftNode = node * 2;
+  int rightNode = node * 2 + 1;
+  update(start, mid, leftNode, index, value);
+  update(mid + 1, end, rightNode, index, value);
+  tree[node] = tree[leftNode] + tree[rightNode];
+}
+
+void printTree() {
+  for (int i = 0; i < n * 3; i++) {
+    cout << tree[i] << ' ';
+  }
+  cout << '\n';
 }
 
 int main() {
@@ -41,11 +58,18 @@ int main() {
     arr[i] = {i, value};
   }
   sort(arr, arr + n, compare);
-
+  long long swap = 0;
   for (int i = 0; i < n; i++) {
     int from = arr[i].first + 1;
     int to = n - 1;
-    cout << "from: " << from << " to: " << to << '\n';
+    // cout << "from: " << from << " to: " << to << ' '
+    //      << "value: " << arr[i].second << ' ';
+
+    swap += get(0, n - 1, 1, from, to);
+    // cout << "swap: " << swap << '\n';
+    update(0, n - 1, 1, arr[i].first, 1);
+    // printTree();
   }
+  cout << swap << '\n';
   return 0;
 }
