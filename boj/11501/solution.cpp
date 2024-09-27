2e5565fc4d7c2745dd15915559c376f11c169fcc
kimjihyo <kimjihyo0325@gmail.com>
2024-09-27 15:39:19 +0000

    ac

--- boj/11501/solution.cpp
+++ boj/11501/solution.cpp
@@ -1,6 +1,12 @@
+#include <algorithm>
 #include <iostream>
+#include <queue>
 #include <vector>
 using namespace std;
+
+bool compare(pair<int, int> &a, pair<int, int> &b) {
+  return a.second > b.second;
+}
 
 int main() {
   ios::sync_with_stdio(0);
@@ -11,23 +17,33 @@ int main() {
     int n;
     cin >> n;
     vector<int> arr(n);
-    long long sellAmount = 0;
-    long long buyAmount = 0;
-    int num = 0;
+    vector<pair<int, int>> pairs;
+    queue<pair<int, int>> q;
+    long long num = 0;
+    long long profit = 0;
     for (int i = 0; i < n; i++) {
       cin >> arr[i];
+      pairs.push_back({i, arr[i]});
     }
-    for (int i = 0; i < n - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        sellAmount += arr[i] * num;
+    sort(pairs.begin(), pairs.end(), compare);
+    q.push(pairs[0]);
+
+    for (int i = 1; i < n; i++) {
+      if (q.back().first < pairs[i].first) {
+        q.push(pairs[i]);
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      if (i == q.front().first) {
+        profit += num * arr[i];
+        q.pop();
         num = 0;
       } else {
-        buyAmount += arr[i];
+        profit -= arr[i];
         num++;
       }
     }
-    sellAmount += arr[n - 1] * num;
-    cout << sellAmount - buyAmount << '\n';
+    cout << profit << '\n';
   }
   return 0;
 }
