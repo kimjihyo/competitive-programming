5db4b82bf9fe7dab426c3db3c2a350edd1f453d1
kimjihyo <kimjihyo0325@gmail.com>
2024-10-04 10:25:33 +0000

    ac

--- boj/10844/solution.cpp
+++ boj/10844/solution.cpp
@@ -3,9 +3,33 @@
 using namespace std;
 
 int main() {
-  vector<int> dp[10];
-  for (int i = 1; i < 11; i++) {
-    dp[i].push_back(i);
+  int n;
+  cin >> n;
+  vector<vector<long long>> dp(n + 1, vector<long long>(10, 0));
+  for (int i = 1; i < 10; i++) {
+    dp[1][i] = 1;
   }
+  for (int i = 1; i < n; i++) {
+    for (int j = 0; j < 10; j++) {
+      if (j == 0) {
+        dp[i + 1][j + 1] += dp[i][j];
+        dp[i + 1][j + 1] %= 1000000000;
+      } else if (j == 9) {
+        dp[i + 1][j - 1] += dp[i][j];
+        dp[i + 1][j - 1] %= 1000000000;
+      } else {
+        dp[i + 1][j - 1] += dp[i][j];
+        dp[i + 1][j + 1] += dp[i][j];
+        dp[i + 1][j - 1] %= 1000000000;
+        dp[i + 1][j + 1] %= 1000000000;
+      }
+    }
+  }
+  long long ans = 0;
+  for (int j = 0; j < 10; j++) {
+    ans += dp[n][j];
+    ans %= 1000000000;
+  }
+  cout << ans << '\n';
   return 0;
 }
