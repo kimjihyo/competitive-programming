6177e246e1a2d65f06d35c9b961d95d5fb842203
kimjihyo <kimjihyo0325@gmail.com>
2024-10-05 05:40:03 +0000

    ac

---
+++ boj/1904/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> dp(n + 1);
+  dp[1] = 1;
+  dp[2] = 2;
+  for (int i = 3; i < n + 1; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+    dp[i] %= 15746;
+  }
+  cout << dp[n] << '\n';
+  return 0;
+}
