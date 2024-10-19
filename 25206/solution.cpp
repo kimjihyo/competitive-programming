9ccc93217c75ec3235d9442e4b18d37489fa62b4
kimjihyo <kimjihyo0325@gmail.com>
2024-10-19 03:43:25 +0000

    ac

---
+++ 25206/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+  unordered_map<string, double> dict = {{"A+", 4.5}, {"A0", 4.0}, {"B+", 3.5},
+                                        {"B0", 3.0}, {"C+", 2.5}, {"C0", 2.0},
+                                        {"D+", 1.5}, {"D0", 1.0}, {"F", 0}};
+
+  double sum = 0;
+  double sum_credit = 0;
+  for (int i = 0; i < 20; i++) {
+    string subject, grade;
+    double credit;
+    cin >> subject >> credit >> grade;
+    if (grade != "P") {
+      sum += (credit * dict.find(grade)->second);
+      sum_credit += credit;
+    }
+  }
+  cout << sum / sum_credit << '\n';
+}
