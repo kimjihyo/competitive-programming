521b1b8537bb9df8825aabb3008204703c7fd0e7
kimjihyo <kimjihyo0325@gmail.com>
2024-10-31 09:28:16 +0000

    ac

---
+++ boj/2747/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> arr(n + 1);
+  arr[0] = 0;
+  arr[1] = 1;
+
+  for (int i = 2; i < n + 1; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+
+  cout << arr[n] << '\n';
+
+  return 0;
+}
